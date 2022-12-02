01508803486916840873552a903c2e5169d3d39e
hopanoid <65086741+hopanoid@users.noreply.github.com>
2022-12-02 14:39:58 +0100

    support for chemfig packges added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,12 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# Option for Latex
+latex_elements = {
+    'extrapackages': r'\usepackage{chemfig}'
+}
+
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
