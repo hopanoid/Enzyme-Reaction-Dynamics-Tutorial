df30940ea1b68bff9d35604d859c55b3a74da3d1
hopanoid <65086741+hopanoid@users.noreply.github.com>
2022-12-02 17:14:12 +0100

    Support for mathjx added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,6 @@ extensions = [
     'sphinx.ext.mathjax',
 ]
 
-extensions.append('sphinx_execute_code')
-
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
